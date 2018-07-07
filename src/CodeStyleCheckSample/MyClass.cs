4e47598a0832154990512fdc5e83491b72bd292f
Mark Vincze <mrk.vincze@gmail.com>
2018-07-07 13:11:16 +0200

    Initial commit without style checking

---
+++ src/CodeStyleCheckSample/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CodeStyleCheckSample
+{
+    public class MyClass
+    {
+        public void foo()
+        {
+            // Block without braces
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+                Console.WriteLine("Yay, it's Friday!");
+
+            // If without a trailing space
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                Console.WriteLine("It's Monday (︶︿︶)");
+            }
+
+            // Wrong indentation
+                Console.WriteLine("Bar");
+            
+            // Tabs instead of spaces
+        	Console.WriteLine("Baz");
+        }
+    }
+
+    public class wrongCasing
+    {
+        // Public field
+        public string PublicField = "Foo";
+    }
+}
